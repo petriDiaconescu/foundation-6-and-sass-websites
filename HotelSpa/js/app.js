1256f29e2a4d49f8b3d57073ecf063f068210ca1
petrero <petrero@gmail.com>
2016-09-26 19:39:54 +0300

    Create about us page. Create a template for future pages. Apply a dynamic height for #overlay div function of the height of the video or the image it contents using javascript. Make about us in .headliner div to disappear on small screens. Now we have a problem with the height of the #overlay div that doesn't adapt with the media screen.

--- HotelSpa/js/app.js
+++ HotelSpa/js/app.js
@@ -1,4 +1,10 @@
+function getHeight(){
+  var height = $('section.promo-video, section.promo').height();
+  $('#overlay').css({'height': height + 'px'});
+}
+
 $(function(){
+  getHeight();
   $('#gallery a').on('click', function(){
     var image = $(this).find('img').attr('src');
     var imageSelected = image.split('thumbnail');
