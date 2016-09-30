e360335f40f5932a9acb8ab9b8df607e3f7b372c
petrero <petrero@gmail.com>
2016-09-30 22:39:28 +0300

    When media screen devices is small or become small remove expanded class from the main menu so horizontal menu is transformed into vertical menu on small devices. Use some piece of javascript to do this.

--- FrontEndStore/src/assets/js/app.js
+++ FrontEndStore/src/assets/js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation();
+
+$(window).on('changed.zf.mediaquery', function(event, newSize, oldSize){
+  //console.log('The new size is:' + newSize);
+  //console.log('The old size is:' + oldSize);
+  if(newSize === 'small' && oldSize === 'medium'){
+    $('#main-menu ul').removeClass('expanded');
+  }
+  if(newSize === 'medium' && oldSize === 'small'){
+    $('#main-menu ul').addClass('expanded');
+  }
+});
+
+var size = Foundation.MediaQuery.current;
+if(size === 'small'){
+  $('#main-menu ul').removeClass('expanded');
+}
