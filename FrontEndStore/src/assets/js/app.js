7f624853f92ebbae0fed7934bc997bdbcc7b5c9c
petrero <petrero@gmail.com>
2016-10-02 18:23:15 +0300

    Tracking scrolling on website with javascript. When scroll more than 300 the main navigation get sticked on top of the browser window. We add a class fixed with javascript and style it in custom.scss to have main navigation sticked on top of the browser window when scrolling more than a certain number and remove the class when scroll at the top of page.

--- FrontEndStore/src/assets/js/app.js
+++ FrontEndStore/src/assets/js/app.js
@@ -13,7 +13,21 @@ $(window).on('changed.zf.mediaquery', function(event, newSize, oldSize){
   }
 });
 
+//Takes the current size of the screen
 var size = Foundation.MediaQuery.current;
 if(size === 'small'){
   $('#main-menu ul').removeClass('expanded');
 }
+
+//Tracks current position when scrolling
+var navigation = $('#main-navigation');
+var nav = new Foundation.Magellan(navigation);
+
+$(window).scroll(function(){
+  //console.log(nav.scrollPos);
+  if(nav.scrollPos >= 300){
+    $(navigation).addClass('fixed');
+  } else {
+    $(navigation).removeClass('fixed');
+  }
+});
