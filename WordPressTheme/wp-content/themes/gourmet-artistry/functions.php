9e596343b9ed7391bd1e4ec77b1df40a0d41b947
petrero <petrero@gmail.com>
2016-10-04 23:26:21 +0300

    Enqueuing the foundation stylesheet and javascript files. The last parameter from wp_enqueue_script shows where the script is embedded. True shows the script is inserted in the footer and false in the header. style.css file is not necessary because we are using foundation framework. We can't remove the file but we removed the wp_enqueue_style associated to this file.

--- WordPressTheme/wp-content/themes/gourmet-artistry/functions.php
+++ WordPressTheme/wp-content/themes/gourmet-artistry/functions.php
@@ -102,11 +102,19 @@ add_action( 'widgets_init', 'gourmet_artistry_widgets_init' );
  * Enqueue scripts and styles.
  */
 function gourmet_artistry_scripts() {
-	wp_enqueue_style( 'gourmet-artistry-style', get_stylesheet_uri() );
-
+  wp_enqueue_style( 'foundation-style', get_template_directory_uri() . '/css/app.css');
+  
 	wp_enqueue_script( 'gourmet-artistry-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
+  
+  wp_enqueue_script( 'jquery' );
+  
 	wp_enqueue_script( 'gourmet-artistry-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+  
+  wp_enqueue_script('foundation-js', get_template_directory_uri() . '/js/foundation.js', array('jquery'), '20151215', true );
+  
+  wp_enqueue_script('what-input', get_template_directory_uri() . '/js/what-input.min.js', array(), '20151215', true );
+  
+  wp_enqueue_script('app-js', get_template_directory_uri() . '/js/app.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
