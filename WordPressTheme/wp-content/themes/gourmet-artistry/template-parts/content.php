0156695f22309794159689c8777b976f1644ebd6
petrero <petrero@gmail.com>
2016-10-08 09:44:49 +0300

    Put thumbnails of the post entries aside the post text content like in our psd template.

--- WordPressTheme/wp-content/themes/gourmet-artistry/template-parts/content.php
+++ WordPressTheme/wp-content/themes/gourmet-artistry/template-parts/content.php
@@ -9,22 +9,27 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+<article id="post-<?php the_ID(); ?>" <?php post_class('row'); ?>>
+  <div class="large-6 columns">
+    <?php the_post_thumbnail('entry'); ?>
+  </div>
+  
+  <div class="large-6 columns">
+    <header class="entry-header">
+      <?php
+      if ( is_single() ) :
+        the_title( '<h1 class="entry-title">', '</h1>' );
+      else :
+        the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+      endif;
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php gourmet_artistry_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+      if ( 'post' === get_post_type() ) : ?>
+      <div class="entry-meta">
+        <?php gourmet_artistry_posted_on(); ?>
+      </div><!-- .entry-meta -->
+      <?php
+      endif; ?>
+    </header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
@@ -40,7 +45,7 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-
+  </div>
 	<footer class="entry-footer">
 		<?php gourmet_artistry_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
