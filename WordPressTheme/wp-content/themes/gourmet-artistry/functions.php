19e78fdc46fcbbe4bc6ce3ba6d4d583b38b40013
petrero <petrero@gmail.com>
2016-10-08 12:06:31 +0300

    Add a custom widget to our sidebar.   We procedded like this. Create widgets.php file in inc folder. Copy from https://codex.wordpress.org/Widgets_API site the content of the example provide on this page and paste it in our new created file. Put php tag as the first text of the file. Copy register_foo_widget() function provided on the mentioned site page. Paste it under the previous pasted code in widgets.php file. Register our custom widgets in functions.php file. In admin area of our WordPress site we see now 'Widget Title' article in 'Available Widgets' section. Drag this article over the 'Sidebar' section at the right side of our admin area. Put some text in 'Title:' text input of the form and click 'Save' link like button.    The sidebar of our refreshed wordpress site has now an image and our custom widget displayed.

--- WordPressTheme/wp-content/themes/gourmet-artistry/functions.php
+++ WordPressTheme/wp-content/themes/gourmet-artistry/functions.php
@@ -157,3 +157,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Widgets
+ */
+require get_template_directory() . '/inc/widgets.php';
