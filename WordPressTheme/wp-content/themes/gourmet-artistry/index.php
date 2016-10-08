94ed15a1c5eceb547f035f631c2bb54b73f7b6af
petrero <petrero@gmail.com>
2016-10-08 09:19:19 +0300

    Adding the main foundation classes for the grid to resemble to our psd template.

--- WordPressTheme/wp-content/themes/gourmet-artistry/index.php
+++ WordPressTheme/wp-content/themes/gourmet-artistry/index.php
@@ -15,42 +15,46 @@
 get_header(); ?>
 
   <?php get_template_part('template-parts/slider', 'entries'); ?>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main"> 
-		<?php
-		if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+  <div class="row">
+    <div id="primary" class="content-area medium-8 columns">
+      <main id="main" class="site-main" role="main"> 
+      <?php
+      if ( have_posts() ) :
 
-			<?php
-			endif;
+        if ( is_home() && ! is_front_page() ) : ?>
+          <header>
+            <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+          </header>
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+        <?php
+        endif;
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+        /* Start the Loop */
+        while ( have_posts() ) : the_post();
 
-			endwhile;
+          /*
+           * Include the Post-Format-specific template for the content.
+           * If you want to override this in a child theme, then include a file
+           * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+           */
+          get_template_part( 'template-parts/content', get_post_format() );
 
-			the_posts_navigation();
+        endwhile;
 
-		else :
+        the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+      else :
 
-		endif; ?>
+        get_template_part( 'template-parts/content', 'none' );
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+      endif; ?>
+
+      </main><!-- #main -->
+    </div><!-- #primary -->
+  
 
 <?php
-get_sidebar();
+    get_sidebar();
+  echo "</div>";
 get_footer();
