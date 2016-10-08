7d2e699d6e93802f71aadafced2ddda5b27f26c5
petrero <petrero@gmail.com>
2016-10-08 15:05:48 +0300

    Add a social media menu in the footer.

--- WordPressTheme/wp-content/themes/gourmet-artistry/footer.php
+++ WordPressTheme/wp-content/themes/gourmet-artistry/footer.php
@@ -22,7 +22,23 @@
             'items_wrap' => '<ul id="%1$s" class="%2$s vertical medium-horizontal menu text-center">%3$s</ul>'
         )) ?>
       </div>
-      
+      <div class="medium-6 columns">
+      <?php 
+        wp_nav_menu(array(
+          'theme_location' => 'social-menu',
+          'container' => 'div',
+          'container_id' => 'menu-social',
+          'container_class' => 'menu',
+          'menu_id' => 'social-menu',
+          'menu_class' => 'menu-items',
+          'depth' => 1,
+          'link_before' => '<span class="screen-reader-text">',
+          'link_after' => '</span>',
+          'fallback_cb' => '',
+          'items_wrap' => '<ul id="%1$s" class="%2$s menu float-right">%3$s</ul>' 
+        )) 
+      ?>
+      </div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
