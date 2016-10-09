1504f902ae387e346c23ffb7cf63cf6884ccb6e2
petrero <petrero@gmail.com>
2016-10-09 12:54:19 +0300

    Extending the customizer. Create some settings and add an action in customizer.php file so as we may modify the background of our header using the administrative wordpress area of our site.   To see the effect of this code we must click 'Customize' link from the top menu of our home site or 'Appearance','Themes','Customize' if you log in http:localhost/wp-admin and click on 'Colors' link from the side menu. Before the code there you may set 'Background Color' and 'Header Text Color'. After writing the code you have a new settings option for 'Header Background Color' as is displayed in sidebar now.   Alternative sites for foundation integration with wordpress may be https://foundationpress.olefredrik.com/, http://jointswp.com/

--- WordPressTheme/wp-content/themes/gourmet-artistry/inc/customizer.php
+++ WordPressTheme/wp-content/themes/gourmet-artistry/inc/customizer.php
@@ -14,9 +14,36 @@ function gourmet_artistry_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+  $wp_customize->add_setting( 
+    'ga_header_color',
+    array(
+      'default' => '#FFFFFF'    
+    )      
+  );
+  $wp_customize->add_control(
+    new WP_Customize_Color_Control(
+        $wp_customize,
+        'link_color',
+        array(
+          'label' => __('Header Background Color'),
+          'section' => 'colors',
+          'settings' => 'ga_header_color'  
+        )    
+    )        
+  );
 }
 add_action( 'customize_register', 'gourmet_artistry_customize_register' );
 
+function ga_customizer_header(){
+  ?>
+    <style type="text/css">
+      header.site-header .no-image {
+        background-color: <?php echo get_theme_mod('ga_header_color') ?>;
+      }
+    </style>
+  <?php         
+};
+add_action('wp_head', 'ga_customizer_header');
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
