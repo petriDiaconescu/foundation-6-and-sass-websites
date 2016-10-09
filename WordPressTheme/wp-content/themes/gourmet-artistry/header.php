4b67e4f98f5ac570908d4a758e252d484f40583b
petrero <petrero@gmail.com>
2016-10-09 08:57:35 +0300

    Working with page templates. Put some content in 'About Us' page and style this page.    To put a content in 'Abiut Us' page we proceed like so. Log in into admin area localhost/wp-admin. Click side menu link 'Pages', submenu 'All Pages'. Hover over the page you want to edit. When you are over it click the edit link that appear. In textarea below 'Add Media' link like button write or paste desired content.

--- WordPressTheme/wp-content/themes/gourmet-artistry/header.php
+++ WordPressTheme/wp-content/themes/gourmet-artistry/header.php
@@ -27,15 +27,8 @@
     <?php if ( get_header_image() ) : ?>
       <div class="header-image" style="background-image: url(<?php header_image(); ?>);">
         <div class="text-center site-branding">
-          <?php
-          if ( is_front_page() && is_home() ) : ?>
-            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-          <?php else : ?>
-            <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-          <?php
-          endif;
-
-          $description = get_bloginfo( 'description', 'display' );
+          <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+          <?php $description = get_bloginfo( 'description', 'display' );
           if ( $description || is_customize_preview() ) : ?>
             <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
           <?php
